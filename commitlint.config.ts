1d3b0510e706326f67d7375349a458b8447d7aac
Igor Silva <igorsilvauk@gmail.com>
2024-08-21 18:56:36 +0100

    Removed lint commit checker

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,8 +3,12 @@ import type { UserConfig } from "@commitlint/types";
 const Configuration: UserConfig = {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
-		"body-max-length": [1, "always", 100],
-		"body-max-line-length": [1, "always", 100],
+		// "body-max-length": [1, "always", 100],
+		// "body-max-line-length": [1, "always", 100],
+		// Disable specific rules
+		"header-max-length": [0], // Example: Disable header length check
+		"type-empty": [0], // Example: Disable type empty check
+		"subject-empty": [0], // Example: Disable subject empty check
 	},
 };
 
