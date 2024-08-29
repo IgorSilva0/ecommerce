89326d5f3160485e29a68887f5401f5c4f382c3f
Igor Silva <igorsilvauk@gmail.com>
2024-08-29 18:52:51 +0100

    fixed NavMobile to use YnsLink instead of Link (Consistant)

--- src/ui/nav/NavMobile.tsx
+++ src/ui/nav/NavMobile.tsx
@@ -1,34 +1,28 @@
-import {
-	RiHome4Line,
-	//RiHome4Fill,
-	RiMenuSearchLine,
-	//RiMenuSearchFill,
-	RiUser3Line,
-	//RiUser3Fill,
-} from "react-icons/ri";
-import Link from "next/link";
+import { RiHome4Line, RiMenuSearchLine, RiUser3Line } from "react-icons/ri";
+import { YnsLink } from "@/ui/YnsLink";
 import { CartSummaryNav } from "@/ui/nav/CartSummaryNav";
 
+const ICON_SIZE = 24;
+
 export const NavMobile = () => {
-	const iconsSize = 24;
 	return (
 		<nav className="sticky -bottom-1 flex w-full justify-between bg-white px-4 pb-5 pt-3 text-xs md:hidden">
-			<Link href={"/"} className="flex flex-col items-center">
-				<RiHome4Line size={iconsSize} />
+			<YnsLink href="/" className="flex flex-col items-center" aria-label="Home">
+				<RiHome4Line size={ICON_SIZE} />
 				<p>Home</p>
-			</Link>
-			<Link href={"/products"} className="flex flex-col items-center">
-				<RiMenuSearchLine size={iconsSize} />
-				Shop
-			</Link>
-			<div className="flex flex-col items-center">
+			</YnsLink>
+			<YnsLink href="/products" className="flex flex-col items-center" aria-label="Shop">
+				<RiMenuSearchLine size={ICON_SIZE} />
+				<p>Shop</p>
+			</YnsLink>
+			<div className="flex flex-col items-center" aria-label="Cart">
 				<CartSummaryNav />
-				Cart
+				<p>Cart</p>
 			</div>
-			<Link href={"/myaccount"} className="flex flex-col items-center">
-				<RiUser3Line size={iconsSize} />
-				Account
-			</Link>
+			<YnsLink href="/myaccount" className="flex flex-col items-center" aria-label="Account">
+				<RiUser3Line size={ICON_SIZE} />
+				<p>Account</p>
+			</YnsLink>
 		</nav>
 	);
 };
