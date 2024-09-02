383f91419619c8beaa6cc405e00508f620993eca
Igor Silva <igorsilvauk@gmail.com>
2024-09-02 12:39:00 +0100

    working on my account page

--- src/app/myaccount/page.tsx
+++ src/app/myaccount/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
 import {
 	ChevronLeft,
 	ChevronRight,
@@ -7,28 +5,19 @@ import {
 	CreditCard,
 	File,
 	Home,
-	LineChart,
 	ListFilter,
 	MoreVertical,
-	Package,
 	Package2,
 	PanelLeft,
 	Search,
 	Settings,
 	ShoppingCart,
 	Truck,
-	Users2,
+	ScrollText,
+	HomeIcon,
 } from "lucide-react";
 
 import { Badge } from "@/ui/shadcn/badge";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from "@/ui/shadcn/breadcrumb";
 import { Button } from "@/ui/shadcn/button";
 import {
 	Card,
@@ -49,160 +38,25 @@ import {
 } from "@/ui/shadcn/dropdown-menu";
 import { Input } from "@/ui/shadcn/input";
 import { Pagination, PaginationContent, PaginationItem } from "@/ui/shadcn/pagination";
-import { Progress } from "@/ui/shadcn/progress";
 import { Separator } from "@/ui/shadcn/separator";
 import { Sheet, SheetContent, SheetTrigger } from "@/ui/shadcn/sheet";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/ui/shadcn/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/shadcn/tabs";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/ui/shadcn/tooltip";
+import { TooltipProvider } from "@/ui/shadcn/tooltip";
+import { YnsLink } from "@/ui/YnsLink";
+import { SeoH1 } from "@/ui/SeoH1";
 
 export default function MyAccount() {
 	return (
 		<TooltipProvider>
 			<div className="flex min-h-screen w-full flex-col bg-muted/40">
-				<aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-					<nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-						<Link
-							href="/"
-							className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-						>
-							<Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
-							<span className="sr-only">Your New Store</span>
-						</Link>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Link
-									href="#"
-									className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-								>
-									<Home className="h-5 w-5" />
-									<span className="sr-only">Dashboard</span>
-								</Link>
-							</TooltipTrigger>
-							<TooltipContent side="right">Dashboard</TooltipContent>
-						</Tooltip>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Link
-									href="#"
-									className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-								>
-									<ShoppingCart className="h-5 w-5" />
-									<span className="sr-only">Orders</span>
-								</Link>
-							</TooltipTrigger>
-							<TooltipContent side="right">Orders</TooltipContent>
-						</Tooltip>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Link
-									href="#"
-									className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-								>
-									<Package className="h-5 w-5" />
-									<span className="sr-only">Products</span>
-								</Link>
-							</TooltipTrigger>
-							<TooltipContent side="right">Products</TooltipContent>
-						</Tooltip>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Link
-									href="#"
-									className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-								>
-									<Users2 className="h-5 w-5" />
-									<span className="sr-only">Customers</span>
-								</Link>
-							</TooltipTrigger>
-							<TooltipContent side="right">Customers</TooltipContent>
-						</Tooltip>
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Link
-									href="#"
-									className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-								>
-									<LineChart className="h-5 w-5" />
-									<span className="sr-only">Analytics</span>
-								</Link>
-							</TooltipTrigger>
-							<TooltipContent side="right">Analytics</TooltipContent>
-						</Tooltip>
-					</nav>
-					<nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-						<Tooltip>
-							<TooltipTrigger asChild>
-								<Link
-									href="#"
-									className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-								>
-									<Settings className="h-5 w-5" />
-									<span className="sr-only">Settings</span>
-								</Link>
-							</TooltipTrigger>
-							<TooltipContent side="right">Settings</TooltipContent>
-						</Tooltip>
-					</nav>
-				</aside>
-				<div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-					<header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-						<Sheet>
-							<SheetTrigger asChild>
-								<Button size="icon" variant="outline" className="sm:hidden">
-									<PanelLeft className="h-5 w-5" />
-									<span className="sr-only">Toggle Menu</span>
-								</Button>
-							</SheetTrigger>
-							<SheetContent side="left" className="sm:max-w-xs">
-								<nav className="grid gap-6 text-lg font-medium">
-									<Link
-										href="#"
-										className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-									>
-										<Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-										<span className="sr-only">Acme Inc</span>
-									</Link>
-									<Link
-										href="#"
-										className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-									>
-										<Home className="h-5 w-5" />
-										Dashboard
-									</Link>
-									<Link href="#" className="flex items-center gap-4 px-2.5 text-foreground">
-										<ShoppingCart className="h-5 w-5" />
-										Orders
-									</Link>
-									<Link
-										href="#"
-										className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-									>
-										<LineChart className="h-5 w-5" />
-										Settings
-									</Link>
-								</nav>
-							</SheetContent>
-						</Sheet>
-						<Breadcrumb className="hidden md:flex">
-							<BreadcrumbList>
-								<BreadcrumbItem>
-									<BreadcrumbLink asChild>
-										<Link href="#">Dashboard</Link>
-									</BreadcrumbLink>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator />
-								<BreadcrumbItem>
-									<BreadcrumbLink asChild>
-										<Link href="#">Orders</Link>
-									</BreadcrumbLink>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator />
-								<BreadcrumbItem>
-									<BreadcrumbPage>Recent Orders</BreadcrumbPage>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
+				<div className="flex flex-col sm:gap-4">
+					<header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 dark:bg-slate-800 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 sm:py-4">
+						<YnsLink href="/">
+							<SeoH1 className="-mt-0.5 whitespace-nowrap pr-5 text-xl font-bold">
+								Your New Store
+							</SeoH1>
+						</YnsLink>
 						<div className="relative ml-auto flex-1 md:grow-0">
 							<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
 							<Input
@@ -211,66 +65,72 @@ export default function MyAccount() {
 								className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
 							/>
 						</div>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant="outline" size="icon" className="overflow-hidden rounded-full">
-									<Image
-										src="/icon.png"
-										width={36}
-										height={36}
-										alt="Avatar"
-										className="overflow-hidden rounded-full"
-									/>
+						<Sheet>
+							<SheetTrigger asChild>
+								<Button size="icon" variant="outline" className="">
+									<PanelLeft className="h-6 w-6" />
+									<span className="sr-only">Toggle Menu</span>
 								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align="end">
-								<DropdownMenuLabel>My Account</DropdownMenuLabel>
-								<DropdownMenuSeparator />
-								<DropdownMenuItem>Settings</DropdownMenuItem>
-								<DropdownMenuItem>Support</DropdownMenuItem>
-								<DropdownMenuSeparator />
-								<DropdownMenuItem>Logout</DropdownMenuItem>
-							</DropdownMenuContent>
-						</DropdownMenu>
+							</SheetTrigger>
+							<SheetContent side="right" className="sm:max-w-xs">
+								<nav className="grid gap-6 text-lg font-medium">
+									<YnsLink
+										href="/"
+										className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+									>
+										<Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+										<span className="sr-only">Shop</span>
+									</YnsLink>
+									<YnsLink
+										href="#"
+										className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+									>
+										<Home className="h-5 w-5" />
+										Dashboard
+									</YnsLink>
+									<YnsLink href="#" className="flex items-center gap-4 px-2.5 text-foreground">
+										<ScrollText className="h-5 w-5" />
+										Orders
+									</YnsLink>
+									<YnsLink
+										href="#"
+										className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+									>
+										<ShoppingCart className="h-5 w-5" />
+										Cart
+									</YnsLink>
+									<YnsLink
+										href="#"
+										className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+									>
+										<Settings className="h-5 w-5" />
+										Settings
+									</YnsLink>
+								</nav>
+							</SheetContent>
+						</Sheet>
 					</header>
 					<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
 						<div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-							<div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-								<Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
-									<CardHeader className="pb-3">
-										<CardTitle>Your Orders</CardTitle>
-										<CardDescription className="max-w-lg text-balance leading-relaxed">
-											Introducing Our Dynamic Orders Dashboard for Seamless Management and
-											Insightful Analysis.
-										</CardDescription>
-									</CardHeader>
-									<CardFooter>
-										<Button>Create New Order</Button>
-									</CardFooter>
-								</Card>
+							<div className="flex items-center justify-between">
+								<YnsLink href={"/myaccount"} className="">
+									<HomeIcon className="h-5 w-5 text-primary transition-all hover:text-slate-300" />
+								</YnsLink>
+								<h2 className="text-center text-2xl font-semibold">Order history</h2>
+								<div />
+							</div>
+							<div className="flex justify-evenly">
 								<Card x-chunk="dashboard-05-chunk-1">
-									<CardHeader className="pb-2">
+									<CardHeader className="">
 										<CardDescription>This Week</CardDescription>
-										<CardTitle className="text-4xl">$1,329</CardTitle>
+										<CardTitle className="text-2xl">$1,329</CardTitle>
 									</CardHeader>
-									<CardContent>
-										<div className="text-xs text-muted-foreground">+25% from last week</div>
-									</CardContent>
-									<CardFooter>
-										<Progress value={25} aria-label="25% increase" />
-									</CardFooter>
 								</Card>
 								<Card x-chunk="dashboard-05-chunk-2">
-									<CardHeader className="pb-2">
+									<CardHeader className="">
 										<CardDescription>This Month</CardDescription>
-										<CardTitle className="text-4xl">$5,329</CardTitle>
+										<CardTitle className="text-2xl">$5,329</CardTitle>
 									</CardHeader>
-									<CardContent>
-										<div className="text-xs text-muted-foreground">+10% from last month</div>
-									</CardContent>
-									<CardFooter>
-										<Progress value={12} aria-label="12% increase" />
-									</CardFooter>
 								</Card>
 							</div>
 							<Tabs defaultValue="week">
