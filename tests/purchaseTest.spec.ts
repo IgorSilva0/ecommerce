a2883be0d76ed966d6169f9f39756b9ba06cc4f4
Igor Silva <igorsilvauk@gmail.com>
2024-09-13 19:06:41 +0100

    test updated

--- tests/purchaseTest.spec.ts
+++ tests/purchaseTest.spec.ts
@@ -5,7 +5,7 @@ test("test", async ({ page }) => {
 	await page.locator("a").filter({ hasText: "Reviews" }).first().click();
 	await page.getByRole("button", { name: "Add to cart" }).click();
 	await page.getByRole("link", { name: "Go to payment" }).click();
-	await page.goto("https://ecommerce-rho-woad.vercel.app/cart");
+
 	await page.getByPlaceholder("email@example.com").click();
 	await page.getByPlaceholder("email@example.com").fill("test@gmail.com");
 	await page.getByPlaceholder("••••••••").click();
@@ -20,13 +20,11 @@ test("test", async ({ page }) => {
 	await stripeFrame.getByLabel("Postal code").fill("WA168JN");
 	await stripeFrame.getByPlaceholder("123456").fill("07748 122933");
 
-	await page.getByText("UPS1–3 business days£").click();
-
 	const stripePaymentFrame = page.frameLocator('iframe[src*="inner-payment"]');
 
 	await stripePaymentFrame.getByPlaceholder("1234 1234 1234").fill("4242 4242 4242 42422");
 	await stripePaymentFrame.getByPlaceholder("MM / YY").fill("04 / 42");
 	await stripePaymentFrame.getByPlaceholder("CVC").fill("424");
 
-	expect(page.getByRole("button", { name: "Pay now" }).isVisible());
+	await expect(page.getByRole("heading", { name: "Checkout" })).toBeVisible();
 });
